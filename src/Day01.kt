610d9d694ce878edb31ccca09d80d3592133ecf8
Maksim Dvoretskii <kamerok@gmail.com>
2021-12-01 10:02:43 +0300

    Solve Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,17 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val depth = input.map { it.toInt() }
+        return depth.zipWithNext().count { it.second > it.first }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val depth = input.map { it.toInt() }
+        return depth.windowed(3).zipWithNext().count { it.second.sum() > it.first.sum() }
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(readInput("Day01_test")) == 7)
+    println(part1(readInput("Day01")))
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    check(part2(readInput("Day01_test")) == 5)
+    println(part2(readInput("Day01")))
 }
