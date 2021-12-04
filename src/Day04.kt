0512bd7f0999c0bf6f7f74b864970e5202cf841f
Maksim Dvoretskii <kamerok@gmail.com>
2021-12-04 12:43:42 +0300

    Solve Day04

---
+++ src/Day04.kt
@@ -0,0 +1,75 @@
+fun main() {
+    fun readNumbersAndCards(input: List<String>): Pair<List<Int>, List<Card>> {
+        val numbers = input.first().split(",").map { it.toInt() }
+        val cards: List<Card> = input.drop(1).chunked(6) { chunk ->
+            //skip new line
+            val cardData = (1..5).map { i ->
+                Regex("\\d+").findAll(chunk[i]).map { it.value.toInt() }.toMutableList()
+            }
+            Card(cardData)
+        }
+        return Pair(numbers, cards)
+    }
+
+    fun part1(input: List<String>): Int {
+        val (numbers, cards) = readNumbersAndCards(input)
+        numbers.forEach { number ->
+            cards.forEach { card ->
+                if (card.applyNumberAndCheck(number)) {
+                    return card.uncheckedSum() * number
+                }
+            }
+        }
+        return 0
+    }
+
+    fun part2(input: List<String>): Int {
+        val (numbers, cards) = readNumbersAndCards(input)
+        val remainingCards = cards.toMutableList()
+        var lastWin: Pair<Card, Int>? = null
+        numbers.forEach { number ->
+            remainingCards.removeIf { card ->
+                val cardComplete = card.applyNumberAndCheck(number)
+                if (cardComplete) {
+                    lastWin = card to number
+                }
+                cardComplete
+            }
+        }
+        return lastWin?.let { it.first.uncheckedSum() * it.second } ?: 0
+    }
+
+    check(part1(readInput("Day04_test")) == 4512)
+    println(part1(readInput("Day04")))
+
+    check(part2(readInput("Day04_test")) == 1924)
+    println(part2(readInput("Day04")))
+}
+
+class Card(
+    private val numbers: List<MutableList<Int>>
+) {
+    private var sum = numbers.sumOf { it.sum() }
+
+    fun applyNumberAndCheck(number: Int): Boolean {
+        numbers.forEachIndexed { i, row ->
+            row.forEachIndexed { j, x ->
+                if (x == number) {
+                    sum -= number
+                    numbers[i][j] = -1
+                    if (checkForWin(i, j)) return true
+                }
+            }
+        }
+        return false
+    }
+
+    fun uncheckedSum(): Int = sum
+
+    private fun checkForWin(i: Int, j: Int): Boolean = checkRowForWin(i) || checkColumnForWin(j)
+
+    private fun checkRowForWin(i: Int): Boolean = numbers[i].find { it != -1 } == null
+
+    private fun checkColumnForWin(j: Int): Boolean = numbers.find { it[j] != -1 } == null
+
+}
