2f6916efcdec9c8a3cad32506d1b63f4798cbfb2
Maksim Dvoretskii <kamerok@gmail.com>
2022-12-04 20:39:26 +0100

    Solve Day24 2021

--- src/year2021/day24/Solution.kt
+++ src/year2021/day24/Solution.kt
@@ -1,137 +1,55 @@
 package year2021.day24
 
 import readInput
-import java.math.BigDecimal
-import java.util.ArrayDeque
 
 fun main() {
     val input = readInput(2021, 24).parse()
 
     println(part1(input))
-
-//    println(part2(input))
+    println(part2(input))
 }
 
-private fun part1(input: List<Command>): String {
-    val executor = Executor(input)
-    var number: Long = 99999999999999
-    while (number > 0) {
-        val percent = BigDecimal.ONE - (number.toBigDecimal() / 99999999999999.toBigDecimal())
-        println("${percent.setScale(5)}")
-        val numberText = number.toString()
-        if (numberText.none { it == '0' }) {
-            var cursor = 0
-            if (executor.execute { numberText[cursor].digitToInt().also { cursor++ } } == 0) {
-                return numberText
-            }
-        }
-        number--
+private fun part1(input: List<Parameters>): Long {
+    return solve(input).second
+}
+
+private fun part2(input: List<Parameters>): Long {
+    return solve(input).first
+}
+
+private data class Parameters(val a: Int, val b: Int, val c: Int)
+
+private fun List<String>.parse(): List<Parameters> =
+    chunked(18).map {
+        Parameters(
+            it[4].substringAfterLast(" ").toInt(),
+            it[5].substringAfterLast(" ").toInt(),
+            it[15].substringAfterLast(" ").toInt()
+        )
     }
-    return "not found"
-}
 
-private fun part2(input: List<Context>): Int {
-    return 0
-}
+private fun magicFunction(parameters: Parameters, z: Long, w: Long): Long =
+    if (z % 26 + parameters.b != w) ((z / parameters.a) * 26) + w + parameters.c
+    else z / parameters.a
 
-private fun List<String>.parse(): List<Command> =
-    map { line ->
-        if (line.startsWith("inp")) {
-            Input(Variable.valueOf(line.last().uppercaseChar().toString()))
-        } else {
-            val (operationName, variable, value) = line.split(" ")
-            Operation(
-                on = VariableArgument(Variable.valueOf(variable.uppercase())),
-                with = runCatching {
-                    ValueArgument(value.toInt())
-                }.getOrNull() ?: VariableArgument(Variable.valueOf(value.uppercase())),
-                type = when (operationName) {
-                    "add" -> OperationType.ADD
-                    "mul" -> OperationType.MULTIPLY
-                    "div" -> OperationType.DIVIDE
-                    "mod" -> OperationType.MOD
-                    else -> OperationType.EQUALS
+private fun solve(input: List<Parameters>): Pair<Long, Long> {
+    var zValues = mutableMapOf(0L to (0L to 0L))
+    input.forEach { parameters ->
+        val zValuesThisRound = mutableMapOf<Long, Pair<Long, Long>>()
+        zValues.forEach { (z, minMax) ->
+            (1..9).forEach { digit ->
+                val newValueForZ = magicFunction(parameters, z, digit.toLong())
+                if (parameters.a == 1 || (parameters.a == 26 && newValueForZ < z)) {
+                    zValuesThisRound[newValueForZ] =
+                        minOf(zValuesThisRound[newValueForZ]?.first ?: Long.MAX_VALUE, minMax.first * 10 + digit) to
+                                maxOf(
+                                    zValuesThisRound[newValueForZ]?.second ?: Long.MIN_VALUE,
+                                    minMax.second * 10 + digit
+                                )
                 }
-            )
-        }
-    }
-
-private data class Executor(private val operations: List<Command>) {
-
-    private val cache = mutableMapOf<Pair<Context, Int>, Int>()
-
-    fun execute(readValue: () -> Int): Int {
-        var context = Context()
-        val contexts = mutableListOf(Context())
-        operations.forEachIndexed { index, command ->
-            cache[context to index]?.let { result ->
-                println("hit")
-                if (index > 0) {
-                    contexts.forEachIndexed { i, c -> cache[c to i] = result }
-                }
-                return result
             }
-            context = when (command) {
-                is Input -> context.set(command.variable, readValue())
-                is Operation -> command.execute(context)
-            }
-            contexts.add(context)
         }
-        return (context.values[Variable.Z]
-            ?: 0).also { contexts.forEachIndexed { i, c -> cache[c to i] = it } }
+        zValues = zValuesThisRound
     }
-
-    private fun Operation.execute(context: Context): Context =
-        context.set(on.variable, performOperation(context.resolve(on), context.resolve(with)))
-
-    private fun Operation.performOperation(a: Int, b: Int): Int = when (type) {
-        OperationType.ADD -> a + b
-        OperationType.MULTIPLY -> a * b
-        OperationType.DIVIDE -> a / b
-        OperationType.MOD -> a.mod(b)
-        OperationType.EQUALS -> if (a == b) 1 else 0
-    }
-}
-
-private data class Context(
-    val values: Map<Variable, Int> = mapOf()
-) {
-
-    fun set(variable: Variable, value: Int): Context =
-        Context(values.plus(variable to value))
-
-    fun resolve(argument: Argument): Int = when (argument) {
-        is ValueArgument -> argument.value
-        is VariableArgument -> values.getOrDefault(argument.variable, 0)
-    }
-}
-
-private sealed class Command
-
-private data class Input(
-    val variable: Variable
-) : Command()
-
-private data class Operation(
-    val on: VariableArgument,
-    val with: Argument,
-    val type: OperationType
-) : Command()
-
-private enum class OperationType {
-    ADD, MULTIPLY, DIVIDE, MOD, EQUALS
-}
-
-private sealed class Argument
-
-private data class VariableArgument(
-    val variable: Variable
-) : Argument()
-
-private data class ValueArgument(
-    val value: Int
-) : Argument()
-
-private enum class Variable {
-    W, X, Y, Z
+    return zValues.getValue(0)
 }
